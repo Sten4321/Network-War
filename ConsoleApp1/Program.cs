637d4903bc199660a26b0ea4c52d3856505c28db
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-11-06 16:02:45 +0100

    Asp.net making

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        public static void Testc()
+        {
+            string insertLine = "http://localhost:51286/api/Highscore/5";
+            var client = new HttpClient();
+            var result = await client.GetStringAsync(insertLine);
+        }
+    }
+}
